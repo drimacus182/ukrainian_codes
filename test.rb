f91fe031b3637f6bc2bb587037b74fa09602120a
Vladislav <it.s.not.my@email.com>
2016-07-20 13:35:32 +0300

    IPN implemented, some data to check

--- test.rb
+++ test.rb
@@ -1,12 +1,34 @@
 load 'ukrainian_codes.rb'
 
-codes = File.open('codes.txt').map {|c| c.gsub(/\n+/,'')}
+codes = File.open('edrpou.txt').map {|c| c.gsub(/\n+/,'')}
 
-res = codes.map{|c| UkrainianCodes.edrpou? c}
+res = codes.map{ |c| UkrainianCodes.edrpou? c}
 
 trues = res.count(true)
 falses = res.count(false)
 
+puts "EDRPOU"
+puts "True - #{trues}"
+puts "False - #{falses}"
 
+codes = File.open('ipn_10.txt').map {|c| c.gsub(/\n+/,'')}
+
+res = codes.map{ |c| UkrainianCodes.ipn? c}
+
+trues = res.count(true)
+falses = res.count(false)
+
+puts "IPN 10"
+puts "True - #{trues}"
+puts "False - #{falses}"
+
+codes = File.open('ipn_12.txt').map {|c| c.gsub(/\n+/,'')}
+
+res = codes.map{ |c| UkrainianCodes.ipn? c}
+
+trues = res.count(true)
+falses = res.count(false)
+
+puts "IPN 12"
 puts "True - #{trues}"
 puts "False - #{falses}"
--- ukrainian_codes.rb
+++ ukrainian_codes.rb
@@ -4,12 +4,11 @@ module UkrainianCodes
 	WEIGHTS_OUTER_2 = [3,4,5,6,7,8,9]
 	WEIGHTS_INNER_2 = [9,3,4,5,6,7,8]
 
-
 	def self.edrpou? code_str
 		code_str = code_str.strip
 		return false unless /\A\d{8}\z/.match(code_str)
 
-		code = code_str.split('').map { |c| c.to_i}
+		code = code_str.split('').map { |c| c.to_i }
 		code7 = code[0,7]
 		last = code[7]
 
@@ -35,18 +34,32 @@ module UkrainianCodes
 		end
 
 		summ = weights.each_with_index.map { |w, i| w * code7[i] }.reduce(:+)
-		remainder = summ % 11
+		remainder = (summ % 11) % 10
 
-		return remainder == last if remainder < 10
-		return last == 0 if remainder == 10
-		return false
+		return remainder == last
 	end
 
 
-	def self.ipn? code
+	#####################################################################
 
+	WEIGHTS_IPN_10 = [-1,5,7,9,4,6,10,5,7]
+	WEIGHTS_IPN_12 = [13,17,19,23,29,31,37,41,43,47,53]
+	
+	def self.ipn? code_str
+		code_str = code_str.strip
+		if /\A\d{10}\z/.match(code_str)
+			weights = WEIGHTS_IPN_10
+		elsif /\A\d{12}\z/.match(code_str)
+			weights = WEIGHTS_IPN_12
+		else 
+			return false
+		end
 
+		code = code_str.split('').map { |c| c.to_i }
 
+		summ = weights.each_with_index.map { |w, i| w * code[i] }.reduce(:+)
+		remainder = (summ % 11) % 10
 
+		return remainder == code.last
 	end
 end
